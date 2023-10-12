2f384e3bd70a2569dd83684516382829d527ea73
japc78 <japc.dev@gmail.com>
2023-10-12 23:29:39 +0200

    feat: :sparkles: Customer-product communication

--- businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -1,14 +1,31 @@
 package com.paymentchain.customer.controller;
 
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.paymentchain.customer.entities.Customer;
+import com.paymentchain.customer.entities.CustomerProduct;
 import com.paymentchain.customer.repository.CustomerRepository;
+
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
 
 @RestController
 @RequestMapping("/customer")
@@ -16,6 +33,31 @@ public class CustomerRestController {
 
 	@Autowired
 	CustomerRepository customerRepository;
+
+	private final WebClient.Builder webClientBuilder;
+
+	/**
+	 * @param webClientBuilder
+	 */
+	public CustomerRestController(WebClient.Builder webClientBuilder) {
+		this.webClientBuilder = webClientBuilder;
+	}
+
+	//webClient requires HttpClient library to work propertly
+	HttpClient httpClient = HttpClient.create()
+		//Connection Timeout: is a period within which a connection between a client and a server must be established
+		.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+		.option(ChannelOption.SO_KEEPALIVE, true)
+		.option(EpollChannelOption.TCP_KEEPIDLE, 300)
+		.option(EpollChannelOption.TCP_KEEPINTVL, 60)
+		//Response Timeout: The maximun time we wait to receive a response after sending a request
+		.responseTimeout(Duration.ofSeconds(1))
+		// Read and Write Timeout: A read timeout occurs when no data was read within a certain
+		//period of time, while the write timeout when a write operation cannot finish at a specific time
+		.doOnConnected(connection -> {
+				connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+				connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+		});
 
 	@GetMapping()
 	public ResponseEntity<?> findAll() {
@@ -44,6 +86,9 @@ public class CustomerRestController {
 	@PostMapping()
 	public ResponseEntity<?> create(@RequestBody Customer customer) {
 		try {
+			//A cada producto le asignamos el customer.
+			customer.getProducts().forEach(product -> product.setCustomer(customer));
+
 			customerRepository.save(customer);
 			return new ResponseEntity<>(HttpStatus.OK);
 		} catch (Exception e) {
@@ -77,5 +122,42 @@ public class CustomerRestController {
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+
+	@GetMapping("/full")
+	public ResponseEntity<?> getByCode(@RequestParam String code) {
+		try {
+			Optional<Customer> customer = customerRepository.findByCode(code);
+
+			if (!customer.isPresent()) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+
+			List<CustomerProduct> products = customer.get().getProducts();
+
+			products.forEach(product -> {
+				String productName = getProductName(product.getProductId());
+				product.setProductName(productName);
+			});
+
+			return new ResponseEntity<>(customer, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+
+	private String getProductName(Long id) {
+		WebClient webClient = webClientBuilder.clientConnector(new ReactorClientHttpConnector(httpClient))
+			.baseUrl("http://localhost:8083/product")
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.defaultUriVariables(Collections.singletonMap("url", "http://localhost:8083/product"))
+			.build();
+
+		JsonNode block = webClient.method(HttpMethod.GET).uri("/" + id)
+			.retrieve().bodyToMono(JsonNode.class).block();
+
+		String name = block.get("name").asText();
+		return name;
 	}
 }
--- businessdomain/customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
@@ -1,9 +1,18 @@
 package com.paymentchain.customer.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.paymentchain.customer.entities.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+	// @Query("SELECT c FROM Customer c WHERE c.code = ?1")
+	public Optional<Customer> findByCode(String code);
+
+
+	// @Query("SELECT c FROM Customer c WHERE c.iban = ?1")
+	public Optional<Customer>  findByIban(String iban);
 }
